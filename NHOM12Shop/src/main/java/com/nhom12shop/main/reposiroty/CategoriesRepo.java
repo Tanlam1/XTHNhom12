8a77d92206f9a20b5db867749657c7853fd561d1
Hoang Minh Duy <minhduy12581@gmail.com>
2022-10-28 01:51:28 +0700

    Update CategoriesRepo.java

--- NHOM12Shop/src/main/java/com/nhom12shop/main/reposiroty/CategoriesRepo.java
+++ NHOM12Shop/src/main/java/com/nhom12shop/main/reposiroty/CategoriesRepo.java
@@ -2,6 +2,7 @@ package com.nhom12shop.main.reposiroty;
 
 import com.nhom12shop.main.entity.Categories;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,6 @@ import java.util.List;
 @Repository
 public interface CategoriesRepo extends JpaRepository<Categories, Long> {
 
+	@Query(value = "SELECT * FROM categories WHERE isDeleted = ?", nativeQuery = true)
     List<Categories> findByIsDeleted(Boolean isDeleted);
 }
